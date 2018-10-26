8a8968426b2f01d7a6b097cf19a2ed1100c9be35
Joshua Tagoe <jnt297@nyu.edu>
2018-10-25 22:44:49 -0400

    extra credit, couldn't figure out delta2, sadly

--- rklab/bmgrep.c
+++ rklab/bmgrep.c
@@ -13,8 +13,21 @@
  */
 int*
 init_delta1(const char *pattern) {
+	int *c = malloc(sizeof(int*)*128);
+	int n = strlen(pattern);
+	for(int i = 0; i < 128; i++){
+		c[i] = n;		
+	}
+	for(int x = 0; x < n; x++){
+		printf("%c\n", pattern[x]);
+		
+			c[pattern[x]] = n - x -1;
+			if(pattern[x] == 'a'){
+				printf("test%d\n", x );
+			}
+		}
 	// Your code here. Do not return NULL
-	return NULL; 
+	return c;
 }
 
 /* Let plen = strlen(pattern).
@@ -28,8 +41,22 @@ init_delta1(const char *pattern) {
  * By contrast, in the BM paper, positions start from 1.
  */
 int find_rpr(const char *pattern, int j) {
+	int plen = strlen(pattern);
+	for(int i = 0; i<j; i++){
+		if(pattern[i] != pattern[j]){
+			continue;
+		}
+		else{
+			for(int x= 0; x< plen-j; x++){
+				if(pattern[i+x] != pattern[j+x]){
+					break;
+				}
+			}
+			return i;
+		}
+	}
 	// Your code here. Do not return strlen(pattern), which is never a valid return value.
-	return strlen(pattern);
+	return 1;
 }
 
 /* Allocate, initialize and return the delta2 array according to the BM paper (page 765).
@@ -44,8 +71,16 @@ int find_rpr(const char *pattern, int j) {
  */
 int*
 init_delta2(const char *pattern) {
+	int plen = strlen(pattern);
+	int *c = malloc(sizeof(int*)*plen);
+	for(int j = 0; j < plen; j++){
+		c[j] = plen- find_rpr(pattern,j);
+	}
+
+	printf("%d\n", c[plen-1]);
+
 	// Your code here. Do not return NULL
-	return NULL;
+	return c;
 }
 
 
@@ -62,6 +97,32 @@ init_delta2(const char *pattern) {
  */
 int 
 boyer_moore(const char *pattern, const char *doc, int *n_aligns) {
-	return -1;
+
+	int* delta1 = init_delta1(pattern);
+	int i = 0;
+	int n = strlen(doc);
+	int m = strlen(pattern);
+	int count = 0;
+	while(i<=(n-m)){
+		int match = 1;
+		for(int j = m-1; j>=0; j--){
+			if (pattern[j] != doc[i+j])
+			{
+				int x = delta1[doc[i+j]];
+				i = i +x;
+				match = 0;
+				break;
+			}
+		}
+		if ((match))
+		{
+			count++;
+			/* code */
+		}
+	}
+	
+	if(!count){
+		return -1;
+	}
 }
 
